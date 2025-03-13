b489e79a22361d395650d382741cc4789213997e
Todd Chavez <84374128+ifbwima@users.noreply.github.com>
2025-03-13 13:31:19 +0100

    service worker

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -6,11 +6,10 @@ import { SessionProvider } from "next-auth/react"
 import { ThemeProvider } from "next-themes"
 import { ErrorBoundary } from "react-error-boundary"
 import { Toaster } from "react-hot-toast"
+import { Layout } from "../components/Common"
 
 import { OpenAPI } from "~/api-client"
 import { APPLICATION_TITLE } from "~/utils"
-
-import { Layout } from "../components/Common"
 
 import "~/styles/data-table.css"
 import "~/styles/tailwind.css"
@@ -24,6 +23,9 @@ OpenAPI.WITH_CREDENTIALS = true
 const MyApp = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => (
   <SessionProvider session={session}>
     <Head>
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" content="#000000" />
+      <link rel="apple-touch-icon" href="/icons/icon1.png" />
       <title>{APPLICATION_TITLE}</title>
       <meta name="A GenAI POC application @ BCG.X" content="By BCG X" />
       <link rel="icon" href="/favicon.ico" />
